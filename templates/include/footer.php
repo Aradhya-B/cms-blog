2dd452ddc65f3482d7d9eda9de773e4878d2019c
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-02-08 13:16:09 -0500

    created reusable html footer

---
+++ templates/include/footer.php
@@ -0,0 +1,7 @@
+<div id="footer">
+        The Uneducated Narcissist &copy; <?php echo date("Y");?>. All rights reserved. <a href="admin.php">Site Admin</a>
+      </div>
+
+    </div>
+  </body>
+</html>
