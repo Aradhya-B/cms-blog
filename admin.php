24525e35bb1c33c3b23554ab2e12024f687d49c4
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-02-08 14:24:40 -0500

    security changes

--- admin.php
+++ admin.php
@@ -40,7 +40,7 @@ function login()
 
     if (isset($_POST['login'])) {
         // The user has posted the login form : attempt to log them in
-        if ($_POST['username'] == ADMIN_USERNAME && password_verify($_POST['password'], PASSWORD_DEFAULT) == ADMIN_PASSWORD) {
+        if ($_POST['username'] == ADMIN_USERNAME && $_POST['password'] == ADMIN_PASSWORD) {
 
             // Login successful : Create a session and redirect to admin homepage
             $_SESSION['username'] = ADMIN_USERNAME;
